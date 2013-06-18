a424f5436e6077a7bd2a961c2f3617353cd59242
habebe <habebe@objectivity.com>
2013-06-18 10:43:12 -0500

    Corrected small errors.

--- root/vertex_operations/f_page_size/__suite__.py
+++ root/vertex_operations/f_page_size/__suite__.py
@@ -11,33 +11,12 @@ table_view = [
 
 plot_view = {
     "plot":[
-        {"name":"rate","data":("object.rate_avg()","math.log(object.page_size(),2)"),"xaxis":"page size = pow(2,x)"},
+        {"name":"rate","data":("object.rate_avg()","object.page_size()"),"xaxis":"page size = pow(2,x)"},
         {"name":"memory","data":("object.memory_used_avg()*1e-6","math.log(object.page_size(),2)"),"xaxis":"page size = pow(2,x)"},
         ],
     "ivar":[
-        {"name":"Database engine","id":"object.engine_id()","content":"object.engine()"},
         {"name":"Platform","id":"object.platform_id()","content":"object.platform()"},
         {"name":"Index Type","id":"object.index_type_id()","content":"object.index_type()"},
-        ]
-    }
-
-search_table_view = [
-    [{"sTitle":"Database engine"},{"content":"object.engine()"}],
-    [{"sTitle":"Page Size"},{"content":"object.page_size()"}],
-    [{"sTitle":"Cache Max(MB)"},{"content":"'%.0fMB'%(1e-3*object.cache_max())"}],
-    [{"sTitle":"Rate (v/s)"},{"content":"'%.2f'%(object.rate_avg())"}],
-    [{"sTitle":"Heap Memory (MB)"},{"content":"'%.3f'%(object.memory_used_avg()*1e-6)"}],
-    
-    ]
-
-search_plot_view = {
-    "plot":[
-        {"name":"rate","data":("object.rate_avg()","math.log(1.0*object.page_size(),2)"),"xaxis":"Page Size = pow(2,x)"},
-        {"name":"memory","data":("object.memory_used_avg()*1e-6","math.log(1.0*object.page_size(),2)"),"xaxis":"Page Size = pow(2,x)"},
-        ],
-    "ivar":[
-        {"name":"Database engine","id":"object.engine_id()","content":"object.engine()"},
-        {"name":"Platform","id":"object.platform_id()","content":"object.platform()"},
         ]
     }
 
