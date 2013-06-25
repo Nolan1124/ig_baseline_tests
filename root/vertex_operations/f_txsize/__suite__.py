4236e971c840cbd4a0059eb17b2c58c89a0bb0c3
habebe <habebe@objectivity.com>
2013-06-25 17:16:30 -0500

    Added query tests.

--- root/vertex_operations/f_txsize/__suite__.py
+++ root/vertex_operations/f_txsize/__suite__.py
@@ -1,9 +1,9 @@
 name = "Transaction Size"
 description = "Vertex Ingestion as a function of transaction size."
 
-txsize = []
+tx_sizes = []
 for i in xrange(8,19):
-    txsize.append(pow(2,i))
+    tx_sizes.append(pow(2,i))
     pass
 
 table_view = [
@@ -27,9 +27,12 @@ plot_view = {
 }
 
 
-graph_size = pow(2,18)*80
-cases = [
-    {
+graph_size = pow(2,18)*10
+query_size = pow(2,17)
+cases = []
+
+for tx_size in tx_sizes:
+    no_index_case = {
         "name":"ingest",
         "description":"Vertex Ingestion as a function of transaction size (page_size=%d)."%(pow(2,14)),
         "type":"vertex_ingest",
@@ -39,17 +42,69 @@ cases = [
             "config":["default:default"],
             "page_size":[14],
             "threads":[1],
-            "use_index":[0,1],
+            "use_index":[0],
             "new":1,
             "size":[graph_size],
-            "txsize":txsize,
-            "txlimit":[10],
+            "txsize":[tx_size],
             "ig_version":["ig.3.0"]
             },
         
         "table_view":table_view,
         "plot_view":plot_view
         }
-    ]
+    cases.append(no_index_case)
+    pass
+
+for tx_size in tx_sizes:
+    index_case = {
+        "name":"ingest",
+        "description":"Vertex Ingestion as a function of transaction size (page_size=%d)."%(pow(2,14)),
+        "type":"vertex_ingest",
+        "data":
+        {
+            "template":["basic"],
+            "config":["default:default"],
+            "page_size":[14],
+            "threads":[1],
+            "use_index":[1],
+            "new":1,
+            "txsize":[tx_size],
+            "size":[graph_size],
+            "ig_version":["ig.3.0"]
+            },
+        "table_view":table_view,
+        "plot_view":plot_view
+        }
+    query_case = {
+        "name":"query",
+        "description":"Vertex Query as a function of transaction size (graph_size=%d,query_size=%d)."%(graph_size,query_size),
+        "type":"query",
+        "data":
+        {
+            "template":["basic"],
+            "vertex":["Node"],
+            "config":["default:default"],
+            "page_size":[14],
+            "threads":[1],
+            "txsize":[tx_size],
+            "size":[query_size],
+            "graph_size":[graph_size],
+            "ig_version":["ig.3.0"]
+            },
+        "table_view":table_view,
+        "plot_view":plot_view
+        }   
+    cases.append(index_case)
+    cases.append(query_case)
     
+
+    
+    
+
+
+    
+
+
+
+
     
