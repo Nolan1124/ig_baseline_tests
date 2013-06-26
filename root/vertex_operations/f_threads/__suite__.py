e3885d488a1877df921769a525f0b9253ec5d27b
habebe <habebe@objectivity.com>
2013-06-26 09:27:41 -0500

    Corrected issues with threading and queries.

--- root/vertex_operations/f_threads/__suite__.py
+++ root/vertex_operations/f_threads/__suite__.py
@@ -22,12 +22,12 @@ plot_view = {
         ]
     }
 
-graph_size = pow(2,18)*10
-query_size = pow(2,17)
+graph_size = pow(2,18)*40
+query_size = pow(2,18)
 tx_size = pow(2,14)
 page_size = 14
 cases = []
-threads = [1,2,3,4,5]
+threads = [1,2,3,4,5,6,7,8]
 
 for thread in threads:
     no_index_case = {
@@ -49,9 +49,54 @@ for thread in threads:
         "table_view":table_view,
         "plot_view":plot_view
         }
-    cases.append(no_index_case)
+    #cases.append(no_index_case)
     pass
 
+index_case = {
+    "name":"ingest",
+    "description":"Vertex Ingestion as a function of number of threads. (page size=%d, transaction size=%d)"%(pow(2,page_size),tx_size),
+    "type":"vertex_ingest",
+    "data":
+    {
+        "template":["basic"],
+        "config":["default:default"],
+        "page_size":[14],
+        "threads":[1],
+        "use_index":[1],
+        "new":1,
+        "txsize":[tx_size],
+        "size":[graph_size],
+        "ig_version":["ig.3.0"]
+        },
+    "table_view":table_view,
+    "plot_view":plot_view
+    }
+cases.append(index_case)
+
+for thread in threads:    
+    query_case = {
+        "name":"query",
+        "description":"Vertex Query as a function of threads (graph_size=%d,query_size=%d)."%(graph_size,query_size),
+        "type":"query",
+        "data":
+        {
+            "template":["basic"],
+            "vertex":["Node"],
+            "config":["default:default"],
+            "page_size":[14],
+            "threads":[thread],
+            "txsize":[tx_size],
+            "size":[query_size],
+            "graph_size":[graph_size],
+            "ig_version":["ig.3.0"]
+            },
+        "table_view":table_view,
+        "plot_view":plot_view
+        }   
+    cases.append(query_case)
+    pass
+
+threads = threads[1:]
 for thread in threads:
     index_case = {
         "name":"ingest",
@@ -72,28 +117,6 @@ for thread in threads:
         "table_view":table_view,
         "plot_view":plot_view
         }
-    query_case = {
-        "name":"query",
-        "description":"Vertex Query as a function of threads (graph_size=%d,query_size=%d)."%(graph_size,query_size),
-        "type":"query",
-        "data":
-        {
-            "template":["basic"],
-            "vertex":["Node"],
-            "config":["default:default"],
-            "page_size":[14],
-            "threads":[thread],
-            "txsize":[tx_size],
-            "size":[query_size],
-            "graph_size":[graph_size],
-            "ig_version":["ig.3.0"]
-            },
-        "table_view":table_view,
-        "plot_view":plot_view
-        }   
     cases.append(index_case)
-    cases.append(query_case)
-    
-
     
     
