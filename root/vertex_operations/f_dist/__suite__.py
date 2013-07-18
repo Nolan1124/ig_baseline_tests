764326801438c191cbc91760f8bc13bac59d1420
Henock Abebe <henocka@Henock-Abebes-MacBook-Pro.local>
2013-07-17 22:34:51 -0700

    Added fake ingest name.

--- root/vertex_operations/f_dist/__suite__.py
+++ root/vertex_operations/f_dist/__suite__.py
@@ -31,9 +31,9 @@ cases = []
 processes = []
 num_proc = 8
 
-def generate_case(_processes_,_graph_size_,_new_):
+def generate_case(_processes_,_graph_size_,_new_,name="ingest"):
     return {
-        "name":"ingest",
+        "name":name,
         "description":"Vertex Ingestion as a function of hosts (page size=%d, transaction size=%d)"%(pow(2,page_size),tx_size),
         "type":"vertex_ingest",
         "data":
@@ -53,7 +53,7 @@ def generate_case(_processes_,_graph_size_,_new_):
         "plot_view":plot_view
         }
 
-init_case = generate_case([([0],1)],1,1) 
+init_case = generate_case([([0],1)],1,1,"initial_ingest") 
 
 #machine 0 only setup
 for i in xrange(num_proc):
@@ -62,42 +62,17 @@ for i in xrange(num_proc):
     cases.append(generate_case(process,graph_size,0))
     pass
 
-
-
-
 #machine 1 only setup
 for i in xrange(num_proc):
-    #process = [([1],i+1)]
-    #cases.append(generate_case_(process,1,1)
+    process = [([1],i+1)]
+    cases.append(init_case)
+    cases.append(generate_case_(process,graph_size,0)
     pass
 
 
 #local+remote machine setup
 for i in xrange(num_proc):
-    #processes.append(([0,1],i+1))
-    pass
-
-if 0:
-    cases.append(
-        {
-            "name":"ingest",
-            "description":"Vertex Ingestion as a function of hosts (page size=%d, transaction size=%d)"%(pow(2,page_size),tx_size),
-            "type":"vertex_ingest",
-            "data":
-            {
-                "template":["basic_non_unique"],
-                "config":["dist_host:host1"],
-                "page_size":[14],
-                "threads":[1],
-                "use_index":[0],
-                "new":1,
-                "txsize":[tx_size],
-                "size":[graph_size],
-                "ig_version":["ig.3.1"],
-                "process":processes,
-                },
-            "table_view":table_view,
-            "plot_view":plot_view
-            }
-        )
+    process = [([0,1],i+1)]
+    cases.append(init_case)
+    cases.append(generate_case_(process,graph_size,0)
     pass
